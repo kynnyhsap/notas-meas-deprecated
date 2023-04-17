95385c1750c55669a5daea8399313767b148908d
Andriy Pashynnyk <tobirawork@gmail.com>
2023-04-17 16:30:06 +0300

    better view on desktop

--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -1,36 +1,24 @@
 <script lang="ts">
   import EpisodesThread from './EpisodeThread.svelte';
-  // import { swipe } from 'svelte-gestures';
-  import { page } from '$app/stores';
-  import { goto } from '$app/navigation';
-  import type { HydratedEpisode } from "./+page.server";
+  import type { HydratedEpisode } from './+page.server';
 
-  function handleSwipe(event: any) {
-    const { direction } = event.detail;
-
-    const daysBefore = Number($page.url.searchParams.get('daysBefore') ?? 0);
-
-    if (direction === 'left') {
-      if (!daysBefore) {
-        return;
-      }
-
-      goto(`?daysBefore=${daysBefore - 1}`);
-    } else if (direction === 'right') {
-      goto(`?daysBefore=${daysBefore + 1}`);
-    }
-  }
-
-  export let data: { feed: HydratedEpisode[]};
+  export let data: { feed: HydratedEpisode[] };
 </script>
 
-<!--<div-->
-<!--  use:swipe={{ timeframe: 300, minSwipeDistance: 60, touchAction: 'pan-y' }}-->
-<!--  on:swipe={handleSwipe}-->
-<!--&gt;-->
-
-<div>
+<div class="feed">
   {#each data.feed as episode}
     <EpisodesThread {episode} />
   {/each}
 </div>
+
+<style lang="postcss">
+  .feed {
+    @apply m-auto max-w-2xl;
+  }
+
+  @media screen and (min-width: 42rem) {
+    .feed {
+      @apply border-l border-r border-l-gray-500 border-r-gray-500;
+    }
+  }
+</style>
